376dd481a1539865294b3efadc7c30e2f0dc94d3
Spencer Moran <smoran02@gmail.com>
2014-04-12 14:26:11 -0700

    maybe this will work?

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var alchemy = new AlchemyAPI('b03910ecf00dceb5040c7ffbb61be5a1cf856aba');
 
 
 exports.index = function(req, res){
-	sentiment.find({}).sort('-date').limit(10).exec(function(err, sents){
+	sentiment.find({}).sort('+date').limit(10).exec(function(err, sents){
 		console.log(sents);
 		res.render('index.ejs', { sentiments: sents });
 	});
